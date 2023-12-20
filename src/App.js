975c9184e634189a2106f99f2b484ae973bdcae7
Victoria S <hturay1@gmail.com>
2023-12-20 10:36:21 -0500

    changes made from theia lab environment

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
-import { AppProvider } from './context/AppContext';
+
 import CartValue from './components/CartValue';
 import ExpenseList from './components/ExpenseList';
 import ItemSelected from './components/ItemSelected';
 import Location from './components/Location';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div className="App">
+        <CartValue/>
+        <ExpenseList/>
+        <ExpenseItem/>
+        <ItemSelected/>
+        <Location/>
     </div>
   );
 }
